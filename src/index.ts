de664f75cc25ff908134637790a229998d1b8faf
Coach Chuck <christian.krueger.health@gmail.com>
2024-04-15 15:23:30 -0600

    starter is now devnet

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { Connection } from '@solana/web3.js';
+import { clusterApiUrl, Connection } from '@solana/web3.js';
 import { initializeKeypair } from '@solana-developers/helpers';
 
 (async () => {
-  const connection = new Connection('http://127.0.0.1:8899', 'finalized');
+  const connection = new Connection(clusterApiUrl('devnet'), 'finalized');
   const payer = await initializeKeypair(connection);
 
 })()
